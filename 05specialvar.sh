87eb56655225f0c4a52c16b46e1ff3cc4006e7fc
satishbandi201 <bandisatish20@gmail.com>
2025-05-20 16:06:49 +0530

    special var

---
+++ 05specialvar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo " all variable passed in script : $@"
+echo " Number of variable passed in script : $#"
+echo " Name of the script : $0"
+echo " Current working Directory : $PWD"
+echo " User running this script : $USER"
+echo " Home Directory of user : $HOME"
+echo " PID of the script : $$"
+sleep 10 &
+echo "PID of last command running in background: $!"
