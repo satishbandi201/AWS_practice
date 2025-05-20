6677284e8f61a99d07d3ee6a5e306f13380e7f0e
satishbandi201 <bandisatish20@gmail.com>
2025-05-20 16:22:08 +0530

    conditions

--- 06contions.sh
+++ 06contions.sh
@@ -2,14 +2,14 @@
 
 DIGIT=$1
 #less
-if [$DIGIT -lt 20];then
+if [$DIGIT -lt 20]then
     echo " given number is less than $DIGIT"
 else
     echo " given number is greater than $DIGIT"
 fi
 
 ##greater
-if [$DIGIT -gt 20];then
+if [$DIGIT -gt 20]then
     echo " given number is greater than $DIGIT"
 else
     echo " given number is less than $DIGIT"
