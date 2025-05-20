70d268dd64dffb23856c2e084d7485ea7d498bfe
satishbandi201 <bandisatish20@gmail.com>
2025-05-20 15:45:29 +0530

    sum condition

--- 03datatypes.sh
+++ 03datatypes.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-num1=10
-num2=20
 dt=$(date)
  echo "script executed at $dt"
+echo "enter first number"
+read -s num1
+echo "enter second number"
+read -s num2
 Total=$(($num1+$num2))
 
 echo "sum of $num1 and $num2 is $Total"
