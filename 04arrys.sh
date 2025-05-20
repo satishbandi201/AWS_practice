1d485f3d28ac3c65b15c65f22beef30fe2de1074
satishbandi201 <bandisatish20@gmail.com>
2025-05-20 15:54:45 +0530

    Bike names

--- 04arrys.sh
+++ 04arrys.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 BIKES=("splendor" "glamour" "FZ" "Unicorn")
 
-echo "First Bike {$BIKE[$1]}"
-echo "Second Bike {$BIKE[$2]}"
-echo "Third Bike {$BIKE[$3]}"
+echo "First Bike ${BIKE[1]}"
+echo "Second Bike ${BIKE[2]}"
+echo "Third Bike ${BIKE[3]}"
 
-echo "All Bikes {$BIKE[$@]}"
-echo "All Bikes {$BIKE[$*]}"
+echo "All Bikes ${BIKE[@]}"
+echo "All Bikes ${BIKE[*]}"
