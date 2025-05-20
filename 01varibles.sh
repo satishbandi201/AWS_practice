a2407bce432f83e66ca757092b1ef8845507f152
satishbandi201 <bandisatish20@gmail.com>
2025-05-20 15:26:20 +0530

    add echo command

--- 01varibles.sh
+++ 01varibles.sh
@@ -2,6 +2,6 @@
 PERSON1=$1
 PERSON2=$2
 
-$PERSON1: Hi $PERSON2, how are you?
-$PERSON2: Hey Hi $PERSON1, I am fine what about you?
-$PERSON1: I am also fine
+echo "$PERSON1: Hi $PERSON2, how are you?"
+echo "$PERSON2: Hey Hi $PERSON1, I am fine what about you?"
+echo "$PERSON1: I am also fine"
