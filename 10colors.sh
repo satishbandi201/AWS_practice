91ed266796f9ae711f1b6d1b1222258eb43c8698
satishbandi201 <bandisatish20@gmail.com>
2025-05-21 16:02:21 +0530

    colors

--- 10colors.sh
+++ 10colors.sh
@@ -14,4 +14,28 @@ then
     echo "you are running with root user"
 else
     echo -e "$R Error: you are not a root user $N"
+    exit 1
 fi
+
+#VALIDATE function
+
+VALIDATE (){
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$2 is installing... $G SUCCESS $N"
+    else
+        echo -e "$2 is installing... $R FAILED $N"
+    fi    
+}
+
+#installing packages
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    VALIDATE $? "mysql"
+else
+    echo "mysql is already installed"
+fi
+
