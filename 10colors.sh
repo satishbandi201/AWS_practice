39c8e898a93c213e97c90ecf3ab7fd7b1f8989af
satishbandi201 <bandisatish20@gmail.com>
2025-05-21 15:45:30 +0530

    colors

---
+++ 10colors.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#user access checking
+
+if [ $USER -eq 0 ] 
+then 
+    echo "you are running with root user"
+else
+    echo "$R Error: you are not a root user $N"
+fi
