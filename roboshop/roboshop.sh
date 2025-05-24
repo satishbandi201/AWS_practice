eb0210b7c38bf8e926d1b06c52b2b58e4078d540
satishbandi201 <bandisatish20@gmail.com>
2025-05-24 15:18:43 +0530

    roboshop

--- roboshop/roboshop.sh
+++ roboshop/roboshop.sh
@@ -9,12 +9,12 @@ DOMAIN_NAME="satish84s.site"
 for instance in ${INSTANCES[@]}
 do
      
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-03198fa0653d6dd89 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=testserver}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-03198fa0653d6dd89 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
  if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-        echo "$insatance IP Adress is : $IP"
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)  
     fi
+    echo "$insatance IP Adress is : $IP"
 done
