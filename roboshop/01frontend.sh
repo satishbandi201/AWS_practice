0b5073a994b8b26abcadc12e905b9c24fbc983f0
satishbandi201 <bandisatish20@gmail.com>
2025-05-27 16:40:58 +0530

    change

--- roboshop/01frontend.sh
+++ roboshop/01frontend.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
 
-USER=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-SCRIPT_DIR=$PWD
-
-##user access checking
-
-if [ $USER -ne 0 ]
-then
-    echo -e "you are running with $G root access $N"
-else
-    echo -e "$R you are not a root user $N"
-    exit 1
-fi
-
-##VALIDATING COMMAND
-
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo "$2 is $G SUCCESS $N"
-    else
-        echo "$2 is $R FAILED $N"
-    fi
-}
+source ./common.sh
 
 ##installing packages
 
