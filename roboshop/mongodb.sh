b3df60cdf56d08a8a852d05e8f7c4c4ffe6ed359
satishbandi201 <bandisatish20@gmail.com>
2025-05-27 17:41:49 +0530

    change

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -4,17 +4,17 @@ source ./common.sh
 
 mongorepo
 
-dnf install mongodb-org -y 
+dnf install mongodb-org -y &>>$LOG_FILE
 VALIDATE $? "installing mongod"
 
-systemctl enable mongod
+systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "enabiling mongod"
 
-systemctl start mongod
+systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "starting mongod"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf 
 VALIDATE $? "replace ip in mongo.conf file"
 
-systemctl restart mongod
+systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "restart mongod service"
