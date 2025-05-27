57bcff2b0f237818c01c88ec5d6c72c3a9c0e1c4
satishbandi201 <bandisatish20@gmail.com>
2025-05-27 18:15:43 +0530

    chahnge

--- roboshop/redis.sh
+++ roboshop/redis.sh
@@ -2,20 +2,20 @@
 
 source ./common.sh
 
-dnf module disable redis -y
+dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "disable redis"
 
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>$LOG_FILE
 VALIDATE $? "enable redis"
 
-dnf install redis -y
+dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "install redis"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
 VALIDATE $? "replace ip in config file"
 
-systemctl enable redis
+systemctl enable redis &>>$LOG_FILE
 VALIDATE $? "enable redis"
 
-systemctl start redis
+systemctl start redis &>>$LOG_FILE
 VALIDATE $? "start redis"
