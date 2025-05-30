65b56214ce1d792b57833f8d4471383f3127c9ee
satishbandi201 <bandisatish20@gmail.com>
2025-05-30 16:11:05 +0530

    change

--- roboshop/rabbitmq.sh
+++ roboshop/rabbitmq.sh
@@ -2,18 +2,24 @@
 
 source ./common.sh
 
-dnf install rabbitmq-server -y
+echo "Please enter rabbitmq password to setup"
+read -s RABBITMQ_PASSWD
+
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATE $? "Adding rabbitmq repo"
+
+dnf install rabbitmq-server -y &>>LOG_FILE
 VALIDATE $? "install rabbitmq"
 
-systemctl enable rabbitmq-server
+systemctl enable rabbitmq-server &>>LOG_FILE
 VALIDATE $? "enable rabbitmq"
 
-systemctl start rabbitmq-server
+systemctl start rabbitmq-server &>>LOG_FILE
 VALIDATE $? "start rabbitmq"
 
-rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl add_user roboshop $RABBITMQ_PASSWD &>>LOG_FILE
 VALIDATE $? "add user"
 
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>LOG_FILE
 VALIDATE $? "set password"
 
