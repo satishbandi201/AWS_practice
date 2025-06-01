ecc8465d44ccd6f51761c69ae84abf0950db75f9
satishbandi201 <bandisatish20@gmail.com>
2025-06-01 13:05:33 +0530

    change

--- roboshop/shipping.sh
+++ roboshop/shipping.sh
@@ -24,10 +24,16 @@ VALIDATE $? "start shipping"
 dnf install mysql -y &>>LOG_FILE
 VALIDATE $? "install mysql"
 
-mysql -h mysql.satish84s.site -uroot -pRoboShop@1 < /app/db/schema.sql &>>LOG_FILE
-mysql -h mysql.satish84s.site -uroot -pRoboShop@1 < /app/db/app-user.sql &>>LOG_FILE
-mysql -h mysql.satish84s.site -uroot -pRoboShop@1 < /app/db/master-data.sql &>>LOG_FILE
-VALIDATE $? "load mysql data"
+mysql -h mysql.daws84s.site -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  &>>$LOG_FILE
+    mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
+    VALIDATE $? "Loading data into MySQL"
+else
+    echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
+fi
 
 systemctl restart shipping &>>LOG_FILE
 VALIDATE $? "restart mysql"
